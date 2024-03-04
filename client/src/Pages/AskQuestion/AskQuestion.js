fd6300ff7557eb99eebbf91df237bc2ca12e9a31
Souvik Nath <souvikn20@gmail.com>
2024-03-04 13:01:01 +0530

    new uer ask question problem

--- client/src/Pages/AskQuestion/AskQuestion.js
+++ client/src/Pages/AskQuestion/AskQuestion.js
@@ -17,8 +17,8 @@ const AskQuestion = () => {
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log({ questionTitle, questionBody, questionTags});
-        console.log(user?.existingUser?.name);
-        dispatch(askQuestion({ questionTitle, questionBody, questionTags, userPosted: user?.existingUser?.name, userId: user?.existingUser?._id || user.newUser?._id}, navigate))
+        console.log(user?.existingUser?.name || user.newUser?.name);
+        dispatch(askQuestion({ questionTitle, questionBody, questionTags, userPosted: user?.existingUser?.name || user.newUser?.name, userId: user?.existingUser?._id || user.newUser?._id}, navigate))
         if (!questionTitle) {
             alert('Title is empty!')
         }
