88e608c7080163b5c52c628ec74734629c206f2c
Souvik Nath <souvikn20@gmail.com>
2024-03-22 12:20:23 +0530

    loading issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 
--- client/src/Components/HomeMainBar/HomeMainBar.js
+++ client/src/Components/HomeMainBar/HomeMainBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './HomeMainBar.css'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import Button from '../Button'
 import QuestionList from './QuestionList'
@@ -8,17 +8,28 @@ import QuestionList from './QuestionList'
 const HomeMainBar = () => {
 
     const questionsList = useSelector(state => state.questionReducer)
-
+    const User = useSelector(state => state.currentUserReducer)
     const location = useLocation()
+    const navigate = useNavigate()
+    
+    const checkAuth = () => {
+        if (User === null) {
+            alert('Login or Signup to ask a question')
+            navigate("/auth")
+        }
+        else{
+            navigate("/askquestions")
+        }
+    }
 
     return (
         <div className='max-w-[calc(100%-330px)] w-[100%] inline-block'>
             <div className='flex justify-between mt-[50px]'>
                 { location.pathname === '/' ? <h1 className='text-4xl font-[400]'>Top Questions</h1> : <h1 className='text-4xl font-[400]'>All Questions</h1> }
-                <Link to='/askquestions'><Button>Ask Questions</Button></Link>
+                <button onClick={checkAuth}><Button>Ask Questions</Button></button>
             </div>
             {
-                questionsList === null ?
+                questionsList.data === null ?
                 <div className='flex justify-center mt-2'>
                     <svg className='animate-spin h-[50px] w-[50px] border-t-2 border-l-2 border-[#009dff] rounded-full' viewBox='0 0 24 24'></svg>
                 </div> :
